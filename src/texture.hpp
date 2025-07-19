272b048a6dc0b3c9342f8320bafe7ec22ed12e63
Jaan Soulier <jaansoulier@gmail.com>
2025-07-19 13:45:36 -0400

    boilerplate

---
+++ src/texture.hpp
@@ -0,0 +1,10 @@
+#pragma once
+
+#include <SDL3/SDL.h>
+
+#include <string_view>
+
+SDL_GPUTextureFormat MppGetDepthTextureFormat(SDL_GPUDevice* device);
+SDL_GPUTexture* MppLoadTexture(SDL_GPUDevice* device, SDL_GPUCopyPass* copyPass, const std::string_view& path);
+SDL_GPUTexture* MppCreateColorTexture(SDL_GPUDevice* device, SDL_Window* window, int width, int height, SDL_GPUTextureUsageFlags usage = 0);
+SDL_GPUTexture* MppCreateDepthTexture(SDL_GPUDevice* device, int width, int height, SDL_GPUTextureUsageFlags usage = 0);
