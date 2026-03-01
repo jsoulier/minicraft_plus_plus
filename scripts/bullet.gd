48bc2fc9c58f5cd1510fb02a4a9b8ed4c6e76a24
Jaan Soulier <jaansoulier@gmail.com>
2026-02-28 23:43:46 -0500

    shooting

--- scripts/bullet.gd
+++ scripts/bullet.gd
@@ -2,7 +2,7 @@ class_name Bullet extends CharacterBody3D
 
 @export var speed: float = 100.0
 	
-func _physics_process(delta: float) -> void:
+func _physics_process(_delta: float) -> void:
 	velocity = -global_basis.z * speed
 	if move_and_slide():
 		queue_free()
--- scripts/player.gd
+++ scripts/player.gd
@@ -1,6 +1,6 @@
 extends Node
 
-@export var rotate_speed: float = 0.001
+@export var mouse_sensitivity: float = 0.001
 @export var camera_offset: Vector3 = Vector3.ZERO
 @export var crosshair_length: float = 10.0
 
@@ -8,6 +8,7 @@ extends Node
 @onready var _camera: Camera3D = $"../Camera3D"
 @onready var _player_crosshair: ColorRect = $"../Control/PlayerCrosshair"
 @onready var _camera_crosshair: ColorRect = $"../Control/CameraCrosshair"
+@onready var _camera_raycast: RayCast3D = $"../Camera3D/RayCast3D"
 
 func _notification(what: int) -> void:
 	if what == NOTIFICATION_WM_WINDOW_FOCUS_OUT:
@@ -18,7 +19,7 @@ func _notification(what: int) -> void:
 func _input(event: InputEvent) -> void:
 	if Input.get_mouse_mode() == Input.MOUSE_MODE_CAPTURED:
 		if event is InputEventMouseMotion:
-			_camera.rotate_y(-event.relative.x * rotate_speed)
+			_camera.rotate_y(-event.relative.x * mouse_sensitivity)
 	if event is InputEventMouseButton:
 		Input.set_mouse_mode(Input.MOUSE_MODE_CAPTURED)
 	elif event.is_action_pressed(&"unfocus"):
@@ -32,18 +33,19 @@ func _process(_delta: float) -> void:
 			var weapon: Weapon = child
 			weapon.fire()
 	
-func _get_screen_position(node: Node3D) -> Vector2:
-	return _camera.unproject_position(node.global_position + -node.global_basis.z * crosshair_length)
-
 func _physics_process(delta: float) -> void:
 	var movement_vector_2d = Input.get_vector("left", "right", "up", "down")
 	var movement_vector = Vector3(movement_vector_2d.x, 0.0, movement_vector_2d.y).normalized()
 	_player.velocity = _player.basis * movement_vector * _player.move_speed
-	var frame_rotate_speed = _player.rotate_speed * delta
-	# TODO: we don't want to align with camera rotation. it should factor camera_offset into account
-	var delta_angle = wrapf(_camera.rotation.y - _player.rotation.y, -PI, PI)
-	_player.rotation.y += clamp(delta_angle, -frame_rotate_speed, frame_rotate_speed)
 	_player.move_and_slide()
-	_camera.global_position = _player.global_position + _player.global_basis * camera_offset
-	_player_crosshair.position = _get_screen_position(_player)
-	_camera_crosshair.position = _get_screen_position(_camera)
+	var rotate_speed = _player.rotate_speed * delta
+	var target_direction = (_camera_raycast.get_collision_point() - _player.global_position).normalized()
+	var target_rotation = atan2(target_direction.z, target_direction.x)
+	var current_direction = _player.global_basis.z
+	var current_rotation = atan2(current_direction.z, current_direction.x)
+	var delta_rotation = wrapf(target_rotation - current_rotation, -PI, PI)
+	_player.rotation.y += clamp(delta_rotation, -rotate_speed, rotate_speed)
+	_camera.global_position = _player.global_position + _camera.global_basis * camera_offset
+	_player_crosshair.position = _camera.unproject_position(_camera_raycast.get_collision_point())
+	_camera_crosshair.position = _camera.unproject_position(_camera.global_position - _camera.global_basis.z)
+	
